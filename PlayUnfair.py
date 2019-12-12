45a35753c5e9fbea308d71183b7369b1c4a586c8
henry <henhenhale@gmail.com>
2019-12-11 18:05:01 -0700

    idk there was a buggy... no one sees my repo right? so no one even sees this message...

--- PlayUnfair.py
+++ PlayUnfair.py
@@ -9,60 +9,59 @@ codeword = raw_input("Enter a codeword: ")
 
 message = raw_input("Enter some text to encrypt or decrypt: ")
 
-#function to create the coded index thing
-def index(codeword) :
+#function to create the coded index
+def indexus(codeword) :
 	#I would like to insert a line that changes codeword from
-	#'apple' to 'aple' so that the index has no repeated letters
-	index = alphabet
+	#'jonathan' to 'jonathn' so that the index has no repeated letters
+	indexC = alphabet
 	#nested for loop that removes letters IN codeword FROM alphabet
 	for x in alphabet :
 		for y in codeword :
 			if x == y :
-				index = index.replace(x, "")
+				indexC = indexC.replace(x, "")
 	#then adds codeword to begginning of alphabet
-	index = codeword + index
+	indexC = codeword + indexC
 	#if codeword=hamlet, index=hamletbcdfgijknopqrsuvwxy
-	print index
-	return index
+	print indexC
+	return indexC
 
 #function to encrypt message with given index
-def encrypt(message, index) :
+def encrypt(message, indexC) :
 	#I needed to remove spaces from and turn lowercase the string
 	message = message.replace(" ", "")
 	message = message.lower()
 	#playfair cipher can only deal with 25 (0-24 for computer) letters
 	#eliminate z
 	message = message.replace("z", "x")
-	#establish ciphertext that we can later add to
+	#establish nil ciphertext that we can later add to
 	ciphertext = ""
+
+	for x in message :
+		pos = indexC.index(x)
+		print pos
+
 	#this is how I parse even and odd locations in the message
 	#[0::2] = start at 0, go till end, move by 2
 	#[1::2] = start at 1, go till end, move by 2
 	even = message [0::2]
 	odd = message [1::2]
 	print even, odd
+
+	#so far everything works
+
 	#here is where playfair math starts
-	for x in even :
-		print ("x " + x)
-		pos = index.index(x)
-		print pos
-		posPlus = index.index(x) #troublesome
-		encPos = 5 * (math.	floor(pos / 5)) + posPlus - 5 * (math.floor(posPlus / 5))
-		if(encPos > 24) :
-					encPos = encPos - 24
-		print pos
-		print posPlus
-		print encPos
-	for x in odd :
-		pos = index.index(x)
-		posNeg = index.index(x) #troublesome
-		encPos = 5 * (math.floor(pos / 5)) + posNeg - 5 * (math.floor(posNeg / 5))
-		if(encPos > 24) :
-					encPos = encPos - 24
-		print pos
-		print posNeg
-		print encPos
-		print ciphertext
+	#for x in even :
+	#	pos = indexC.index(x)
+	#	posPlus = indexC.index(x + 1)	#needs to find position of next letter in message
+	#	encPos = 5 * (math.	floor(pos / 5)) + posPlus - 5 * (math.floor(posPlus / 5))
+	#				#encPos = encPos - 24
+	#for x in odd :
+	#	pos = index.index(x)
+	#	print pos
+	#	posNeg = index.index(x) #troublesome
+	#	encPos = 5 * (math.floor(pos / 5)) + posNeg - 5 * (math.floor(posNeg / 5))
+	#	if(encPos > 24) :
+	#	encPos = encPos - 24
 	return ciphertext
 
-encrypt(message, index(codeword))
+encrypt(message, indexus(codeword))
